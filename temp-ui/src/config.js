8a934365add7315c0f8febfd57d1418d468965e6
ashwini-Calsoft <ashwini.dewade@calsoftinc.com>
2018-09-05 12:01:31 +0530

    added few changes

--- temp-ui/src/config.js
+++ temp-ui/src/config.js
@@ -1,3 +1,3 @@
 // export let invaderServerAddress = 'http://172.17.146.60:8080';
-export let invaderServerAddress = 'http://172.17.146.100:8080';
+export let invaderServerAddress = 'http://172.17.2.28:8081';
 export let tileAppAddress = 'http://172.17.2.28:3000/';
