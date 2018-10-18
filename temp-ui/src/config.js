ef9ae4767107f26bbe49ebffe87e245af3c659b3
Jelson <jelsoncharley@github.com>
2018-10-18 11:16:24 +0530

    Reverting

--- temp-ui/src/config.js
+++ temp-ui/src/config.js
@@ -1,2 +1,2 @@
-//export let invaderServerAddressIP = '172.17.2.28:9092';
-export let invaderServerAddressIP = '172.17.146.60:8080';
+export let invaderServerAddressIP = '172.17.2.28:9092';
+// export let invaderServerAddressIP = '172.17.146.60:8081';
