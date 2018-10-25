0768e0dabac209bd93951d1dc1123fdb6a41bde4
Jelson <jelsoncharley@github.com>
2018-10-25 21:03:27 +0530

    Discovery Modal Bug Fix

--- temp-ui/src/components/DiscoverModal/DiscoverModal.js
+++ temp-ui/src/components/DiscoverModal/DiscoverModal.js
@@ -490,12 +490,12 @@ class DiscoverModal extends Component {
                     <Col sm="2" className='pad'>{item.name ? item.name : '-'}</Col>
                     <Col sm="5" className='pad'>{item.existingInterface.Name} < br />
                         <small>{item.existingInterface.Ip_address ? item.existingInterface.Ip_address : ''}
-                            {item.existingInterface.Subnet ? ' / ' + item.existingInterface.Subnet : ''}
+                            {item.existingInterface.Subnet ? '/' + item.existingInterface.Subnet : ''}
                         </small>
                     </Col>
                     <Col sm="5" className='pad'>{item.actualInterface.Name} < br />
                         <small>{item.actualInterface.Ip_address ? item.actualInterface.Ip_address : ''}
-                            {item.actualInterface.Subnet ? ' / ' + item.actualInterface.Subnet : ''}
+                            {item.actualInterface.Subnet ? '/' + item.actualInterface.Subnet : ''}
                         </small>
                     </Col>
                 </Row>)
